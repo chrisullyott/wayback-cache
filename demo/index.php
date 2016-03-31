c002372e00c1488bc176ed11c7b08da3d4141f58
Chris Ullyott <contact@chrisullyott.com>
2016-03-31 00:28:34 -0700

    A better demo

--- demo/index.php
+++ demo/index.php
@@ -1,35 +1,25 @@
 <?php
 
+	ini_set('display_errors', 1);
 
-	ini_set( "display_errors", 1);
+	// Free Geolocation API
+	// https://freegeoip.net/
+	$endpoint = 'https://freegeoip.net/json/85.90.227.224';
 
-
-	// Breezometer API
-	// http://breezometer.com/api/
-	include('api_keys.php');
-	$lat = '33.966793';
-	$lon = '-118.018911';
-	$endpoint = "http://api-beta.breezometer.com/baqi/?key=$api_key&lat=$lat&lon=$lon";
-
-	// Cache
-	include("../src/cache.class.php");
-	$breeze_data = new Cache(array(
-		"container_path" => "cache",
-		"key" => "breezeometer",
-		"expire" => "minute",
-		"mustMatch" => "/country/",
-		"limit" => 5
+	// Instantiate class
+	include('../src/cache.class.php');
+	$geoIP = new Cache(array(
+		'container_path' => 'cache',
+		'key' => 'geoip',
+		'expire' => 'hourly',
+		'limit' => 10
 	));
-	$breeze = $breeze_data->get($endpoint);
-	$breeze = json_decode($breeze, true);
 
+	// Make request
+	$geoIp = $geoIP->get($endpoint);
+	$geoIp = json_decode($geoIp, true);
 
 	// Output
-	echo "<h3>Air quality data for Whittier, CA:</h3>";
-	echo "<div id='meter' style='width:100px;height:100px;background:".$breeze['breezometer_color']."'>";
-	echo "<div id='aqi' style='color:#fff;font-weight:bold;text-align:center;line-height:100px;font-size:40px;letter-spacing:4px;font-family:Georgia,serif;'>".$breeze['breezometer_aqi']."</div>";
-	echo "</div>";
-	echo "<h4>".$breeze['breezometer_description']."</h4>";
-
+	print_r($geoIp);
 
 ?>
